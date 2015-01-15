2f65cc64b6215c5dd170a729f57ff469655e8a1d
Corentin Smith <corentin.smith@gmail.com>
2015-01-15 12:46:36 +0100

    Add simple skin classifier

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -13,8 +13,46 @@ def get_images(directory):
     return result
 
 
-for imgfile in get_images("ucd-db"):
-    img = cv2.imread(imgfile)
+# for imgfile in get_images("ucd-db"):
+#     img = cv2.imread(imgfile)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+def img_skin_naive(img):
+    """
+    Use simple classifier to detect skin
+    """
+    skin_ratio = 0
+    height, width, depth = img.shape
+
+    for col in img:
+        for pixel in col:
+            skin_ratio += 1 if is_skin_naive(pixel) else 0
+
+    skin_ratio /= float(height * width)
+
+    return skin_ratio
+
+
+def is_skin_naive(pixel):
+    """
+    Simple classifier: return true if the pixel verifies a set of color constraints
+    """
+    r, g, b = pixel
+
+    return (
+        r > 95
+        and g > 40
+        and b > 20
+        and max([r, g, b]) - min([r, g, b]) > 15
+        and abs(r - g) > 15
+        and r > g
+        and r > b
+    )
+
+
+
+img = cv2.imread("skin/1.png")
+print img_skin_naive(img)
