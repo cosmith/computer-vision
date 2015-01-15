5f5422b54397cc4f371de221e67efe933c1a7878
Corentin Smith <corentin.smith@gmail.com>
2015-01-15 12:29:10 +0100

    Add skin/not skin db

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -1,4 +1,20 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("")
+import os
 
+
+def get_images(directory):
+    result = []
+    for f in os.listdir(directory):
+        if f.endswith(".jpg") or f.endswith(".png"):
+            result.append(directory + "/" + f)
+
+    return result
+
+
+for imgfile in get_images("ucd-db"):
+    img = cv2.imread(imgfile)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
